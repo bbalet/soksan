a8b4b895a4ce043f917ae267f38c65046ad04ae2
bbalet <benjamin.balet@gmail.com>
2014-03-01 15:02:11 +0100

    add robustness with the file code
    - Check if the file exists before sending it to the playground
    - clean the path according to OS' DIRECTORY_SEPARATOR

--- php/run.php
+++ php/run.php
@@ -18,8 +18,86 @@
 include 'config.php';
 include 'curl.php';
 
-// Make a custom request by filling body variable with the content of a designated file
-$data = array('version' => '2', 'body' => file_get_contents(SAMPLES_PATH . $_POST['file']));
+$filename = pathJoin(realpath(dirname(__FILE__)), pathClean(SAMPLES_PATH), pathClean($_POST['file']));
 
-// Send a compile request
-echo postPlaygroundRequest('compile', $data);
+//Test if file exists
+if (!file_exists($filename)) {
+	header($_SERVER['SERVER_PROTOCOL'] . 'Internal Server Error', true, 500);
+    echo "file " . $filename . " doesn't exist";
+}
+else {
+	// Make a custom request by filling body variable with the content of a designated file
+	$data = array('version' => '2', 'body' => file_get_contents($filename));
+
+	// Send a compile request
+	echo postPlaygroundRequest('compile', $data);
+}
+
+ /**
+ * This function cleans a path by uniformizing the path separator
+ * which can vary depending on the OS system
+ * @param string $path The path to normalize
+ * @return string Sanitized path
+ */
+function pathClean($path) {
+	$sanitizedPath = str_replace("\\", DIRECTORY_SEPARATOR, $path);
+	$sanitizedPath = str_replace("/", DIRECTORY_SEPARATOR, $sanitizedPath);
+	return $sanitizedPath;
+}
+
+ /**
+ * This function takes a variable amount of strings and joins
+ * them together so that they form a valid file path.
+ * @param array $pieces - The pieces of the file path
+ * @returns string The final file path
+ */
+function pathJoin() {
+	$pieces = array_filter(func_get_args(), function($value) {
+		return $value;
+	});
+	return pathNormalize(implode(DIRECTORY_SEPARATOR, $pieces));
+}
+
+/**
+ * This function takes a valid file path and nomalizes it into
+ * the simplest form possible.
+ * @param string $path The path to normalize
+ * @returns string The normailized path
+ */
+function pathNormalize($path) {
+	if (!strlen($path)) {
+		return ".";
+	}
+
+	$isAbsolute    = $path[0];
+	$trailingSlash = $path[strlen($path) - 1];
+
+	$up     = 0;
+	$pieces = array_values(array_filter(explode(DIRECTORY_SEPARATOR, $path), function($n) {
+		return !!$n;
+	}));
+	for ($i = count($pieces) - 1; $i >= 0; $i--) {
+		$last = $pieces[$i];
+		if ($last == ".") {
+			array_splice($pieces, $i, 1);
+		} else if ($last == "..") {
+			array_splice($pieces, $i, 1);
+			$up++;
+		} else if ($up) {
+			array_splice($pieces, $i, 1);
+			$up--;
+		}
+	}
+
+	$path = implode(DIRECTORY_SEPARATOR, $pieces);
+
+	if (!$path && !$isAbsolute) {
+		$path = ".";
+	}
+
+	if ($path && $trailingSlash == DIRECTORY_SEPARATOR) {
+		$path .= DIRECTORY_SEPARATOR;
+	}
+
+	return ($isAbsolute == DIRECTORY_SEPARATOR ? DIRECTORY_SEPARATOR : "") . $path;
+}
